3aa77767093cd593c99bac680e92ec8d468d0297
Youri van Mill <me@youri.dev>
2023-07-03 12:16:48 +0200

    chore: move externals to variable

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,18 @@
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 
+const external = [
+  '@capacitor/cli/dist/tasks/run',
+  '@capacitor/cli/dist/tasks/build',
+  '@capacitor/cli/dist/config',
+  '@capacitor/cli/dist/tasks/sync',
+  'prompts',
+]
+
 export default [
   {
     input: 'src/index.ts',
-    external: ['@capacitor/cli/dist/tasks/run', '@capacitor/cli/dist/tasks/build', '@capacitor/cli/dist/config', '@capacitor/cli/dist/tasks/sync', 'prompts'],
+    external,
     output: {
       format: 'cjs',
       file: `dist/index.cjs`,
@@ -15,7 +23,7 @@ export default [
   },
   {
     input: 'src/index.ts',
-    external: ['@capacitor/cli/dist/tasks/run', '@capacitor/cli/dist/tasks/build', '@capacitor/cli/dist/config', '@capacitor/cli/dist/tasks/sync', 'prompts'],
+    external,
     output: {
       format: 'es',
       file: `dist/index.mjs`,
